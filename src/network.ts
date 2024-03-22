b282ab93c0c8b759bb283ff1cf15313dd96f7c23
Huima Jose <imoicalda@gmail.com>
2024-03-22 10:48:24 +0100

    Finished network file

--- src/network.ts
+++ src/network.ts
@@ -19,5 +19,12 @@ export class Network {
             console.log("Need at lats two nodes for a file transfer simulation")
             return;
         }
+
+        const sender = this.nodes[0];
+        const reciever = this.nodes[1]
+        const file = new File("example.txt", "This is an example file content")
+        
+        sender.uploadFile(file);
+        sender.downloadFile(reciever, file.name)
     }
 }
