50ec77c3c00b4d4910b696f511384b41ec8f5be8
Huima Jose <imoicalda@gmail.com>
2024-03-22 16:18:46 +0100

    Created registrationService.ts

---
+++ src/registrationService.ts
@@ -0,0 +1,8 @@
+// registrationService.ts
+/**Para começar a implementar a abstração de rede
+ *  e a capacidade de adicionar dinamicamente novos nós,
+ *  podemos criar um serviço de registro simples que permite
+ *  que os nós se registrem e obtenham informações sobre outros
+ *  nós na rede. Aqui está um exemplo de como podemos começar:
+ * 
+ *  */
