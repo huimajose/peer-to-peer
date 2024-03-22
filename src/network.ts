7146904a314523b6bedd53a1e98830fefe103146
Huima Jose <imoicalda@gmail.com>
2024-03-22 10:42:24 +0100

    Created constructor on network file

---
+++ src/network.ts
@@ -0,0 +1,13 @@
+//network
+import { Node } from "./node";
+import { File } from "./file";
+
+export class Network {
+    nodes: Node[];
+
+    constructor() {
+       
+        this.nodes = []
+    }
+    
+}
