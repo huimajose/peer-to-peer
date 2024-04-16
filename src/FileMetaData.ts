57f4ae842c5cc1dc63531a534644ae980adb6d61
Huima Jose <imoicalda@gmail.com>
2024-04-16 16:45:36 +0100

    Created filemetadata file

--- src/FileMetaData.ts
+++ src/FileMetaData.ts
@@ -1,1 +1,42 @@
 import parse from 'parse'
+
+
+export class FileMetaData {
+    fileName: string;
+    filePath: string;
+    fileHash: string;
+    createdAt: Date;
+
+
+    constructor(fileName: string, filePath: string, fileHash: string, createdAt: Date){
+
+
+        this.fileName = fileName;
+        this.filePath = filePath;
+        this.fileHash = fileHash;
+        this.createdAt = createdAt;
+    }
+
+
+
+    async save() {
+        const FileObject = Parse.Object.extend('Files');
+        const fileObject = new FileObject()
+
+
+
+        fileObject.set('fileName', this.fileName);
+        fileObject.set('filePath', this.filePath);
+        fileObject.set('filrHash', this.fileHash);
+        fileObject.set('createdAt', this.createdAt);
+
+        try {
+
+            await fileObject.save();
+            console.log('File metadata saved successfully');
+            
+        } catch (error) {
+            console.error('Error saving file metadata:', error);
+        }
+    }
+}
